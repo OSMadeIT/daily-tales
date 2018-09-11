58dc7da68c3f554a6fe763e3ddad78577c7e4c98
O S <vystoya@gmail.com>
2018-09-11 18:34:17 +0300

    fully crud blog

--- src/main/webapp/scripts/routes/storyRoutes.js
+++ src/main/webapp/scripts/routes/storyRoutes.js
@@ -1,6 +1,74 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var app = angular.module('myApp', ['ui.router']);
 
+
+app.config(function($stateProvider) {
+  var storyState = {
+    name: 'stories',
+    url: '/stories',
+    templateUrl: 'partials/newstory.html',
+  };
+  
+  var editStoryState = {
+    name: 'edit',
+    url: '/edit',
+    templateUrl: 'partials/editstory.html',
+    controller: function($scope, $http){
+        $scope.updateData = function(storoId) {
+        var Id = {id: storoId};
+        var editedStory = {
+            "title": $scope.story.title,
+            "story": $scope.story.story
+        };    
+        $http.put('api/stories/update/'  + Id.id, editedStory);
+
+    };
+    }
+  };
+  
+  var homeState = {
+    name: 'home',
+    url: '/home',
+    templateUrl: 'partials/home.html',
+    controller: function($scope, $http){
+        $http({
+        method : "GET",
+        url : "api/stories/list"
+        }).then(function mySuccess(response) {
+            $scope.stories = response.data;
+        }, function myError(response) {
+            $scope.stories = response.statusText;
+        });
+        }
+  };
+  
+   $stateProvider.state(homeState);
+   $stateProvider.state(storyState);  
+   $stateProvider.state(editStoryState);
+});
+
+app.controller('myCtrl', function($scope, $http, $state) {
+    
+    
+    $scope.sendData = function() {
+ 
+    var storyData = {"title": $scope.storyTitle,
+                "story": $scope.storyBody,
+                "createdAt": "2018-09-10T08:10:04Z[UTC]"
+            };    
+    $http.post('api/stories/create', storyData).success(function (data) {
+     $state.go('home');
+  });
+};
+
+    $scope.deleteData = function(storyId) {
+
+        var storo = { 
+            id: storyId 
+        };    
+        $http.delete('api/stories/delete/'  + storo.id)
+
+};
+
+
+
+});
--- target/mystory-1.0-SNAPSHOT/scripts/controllers/storyController.js
+++ target/mystory-1.0-SNAPSHOT/scripts/controllers/storyController.js
@@ -1,72 +1,86 @@
 var app = angular.module('myApp', ['ui.router']);
 
 
-app.config(function($stateProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
   var storyState = {
     name: 'stories',
     url: '/stories',
     templateUrl: 'partials/newstory.html'
   };
+ 
   
   var editStoryState = {
     name: 'edit',
     url: '/edit',
-    templateUrl: 'partials/editstory.html'
+    templateUrl: 'partials/editstory.html',
+    controller: function($scope, $http, $state){
+        $http({
+        method : "GET",
+        url : "api/stories/list"
+        }).then(function mySuccess(response) {
+            $scope.stories = response.data;
+        }, function myError(response) {
+            $scope.stories = response.statusText;
+        });
+        
+        $scope.updateData = function(storoId, storoTitle, storoStoro) {
+        
+        var Id = {id: storoId};
+        console.log(Id.id);
+        var editedStory = {
+            "title": storoTitle,
+            "story": storoStoro
+        };    
+        console.log(editedStory.title);
+        $http.put('api/stories/update/'  + Id.id, editedStory).then(function(response){
+            $state.go('home');
+        });
+
+    };
+    }
   };
   
   var homeState = {
     name: 'home',
     url: '/home',
-    templateUrl: 'partials/home.html'
-  };
-  
-   $stateProvider.state(homeState);
-   $stateProvider.state(storyState);  
-   $stateProvider.state(editStoryState);
-});
-
-app.controller('myCtrl', function($scope, $http, $state) {
-    $http({
+    templateUrl: 'partials/home.html',
+    controller: function($scope, $http){
+        $http({
         method : "GET",
         url : "api/stories/list"
-    }).then(function mySuccess(response) {
-        $scope.stories = response.data;
-    }, function myError(response) {
-        $scope.stories = response.statusText;
-    });
-    
-    $scope.sendData = function() {
- 
-    var storyData = {"title": $scope.storyTitle,
-                "story": $scope.storyBody,
-                "createdAt": "2018-09-10T08:10:04Z[UTC]"
-            };    
-    $http.post('api/stories/create', storyData).success(function (data) {
-     $state.go('home');
+        }).then(function mySuccess(response) {
+            $scope.stories = response.data;
+        }, function myError(response) {
+            $scope.stories = response.statusText;
+        });
+        }
+  };
+  
+    $urlRouterProvider.otherwise("/home");
+     $stateProvider.state(homeState);
+     $stateProvider.state(storyState);  
+     $stateProvider.state(editStoryState);
   });
-};
+
+    app.controller('myCtrl', function($scope, $http, $state) {
+        $scope.sendData = function() {
+
+        var storyData = {"title": $scope.storyTitle,
+                    "story": $scope.storyBody,
+                    "createdAt": "2018-09-10T08:10:04Z[UTC]"
+                };    
+        $http.post('api/stories/create', storyData).success(function (data) {
+         $state.go('home');
+      });
+    };
 
     $scope.deleteData = function(storyId) {
 
         var storo = { 
             id: storyId 
         };    
-        $http.delete('api/stories/delete/'  + storo.id)
-
+        $http.delete('api/stories/delete/'  + storo.id).success(function (data){
+            $state.go('home');
+        });
 };
-
-    $scope.updateData = function(storoId) {
-        var Id = {id: storoId};
-        var editedStory = {
-            "title": $scope.story.title,
-            "story": $scope.story.story
-        };    
-        $http.put('api/stories/update/'  + Id.id, editedStory).then(function mySuccess(response) {
-        $state.go('/home');
-    }, function myError(response) {
-        $scope.myWelcome = response.statusText;
-      });
-
-    };
-
 });
